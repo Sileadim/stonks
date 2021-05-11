28a56165366d07535113219489de0e92d61cec7c
Sileadim <christopher@omnius.com>
2021-05-11 12:28:46 +0200

    moved stuff

--- train.py
+++
@@ -1,7 +0,0 @@
-from pytorch_lightning.utilities.cli import LightningCLI
-from model import AutoregressiveLstm
-
-from datasets import StocksDataModule
-
-
-cli = LightningCLI(AutoregressiveLstm, StocksDataModule)
