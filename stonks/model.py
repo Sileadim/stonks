b22f9c0504c0fb5b582bf97a3a50be4a396d4551
Sileadim <christopher@omnius.com>
2021-05-12 10:36:05 +0200

    added transformer encoder

--- stonks/model.py
+++ stonks/model.py
@@ -4,51 +4,8 @@ import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader
 import glob
-from torch import Tensor
-
-
-class AutoregressiveLstmSimple(pl.LightningModule):
-    def __init__(self, hidden_size=256, batch_size=4):
-        super().__init__()
-        self.hidden_size = hidden_size
-        self.batch_size = batch_size
-        self.encoder = torch.nn.LSTM(
-            input_size=1, hidden_size=self.hidden_size, num_layers=2, dropout=0.2
-        )
-        self.linear = torch.nn.Linear(self.hidden_size, 1)
-        self.loss_func = torch.nn.MSELoss()
-
-    def forward(self, x):
-
-        # seq_length,batch_size,
-        batch_size, length = x.shape
-        encoded, _ = self.encoder(x.view(length, batch_size, 1).double())
-        out = self.linear(encoded[-1, :, :].view(batch_size, self.hidden_size))
-        return out
-
-    def training_step(self, batch, batch_idx):
-
-        x = batch[:, 0:-1]
-        y = batch[:, -1:]
-        y_pred = self.forward(batch)
-        loss = self.loss_func(y, y_pred)
-        self.log(
-            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        )
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        x = batch[:, 0:-1]
-        y = batch[:, -1:]
-        y_pred = self.forward(batch)
-        loss = self.loss_func(y, y_pred)
-        self.log(
-            "val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        )
-        return loss
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.001)
+from torch import Tensor, nn
+import math
 
 
 class AutoregressiveLstm(pl.LightningModule):
@@ -99,21 +56,29 @@ class AutoregressiveLstm(pl.LightningModule):
 
 
 class PositionalEncoding(pl.LightningModule):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(self, hidden_size, dropout=0.1, max_len=1000):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
-        pe = torch.zeros(max_len, d_model)
+        self.hidden_size = hidden_size
+        pe = torch.zeros(max_len, self.hidden_size)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(
-            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+            torch.arange(0, self.hidden_size, 2).float()
+            * (-math.log(10000.0) / self.hidden_size)
         )
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        # pe = pe.unsqueeze(0).transpose()
         self.register_buffer("pe", pe)
 
     def forward(self, x):
-        x = x + self.pe[: x.size(0), :]
+
+        batch_size, length = x.shape
+
+        positional_encodings = (
+            self.pe[:length, : self.hidden_size - 1].view(1, length, -1).repeat(2, 1, 1)
+        )
+        x = torch.cat((x.view(batch_size, length, -1), positional_encodings), axis=2)
         return self.dropout(x)
 
 
@@ -131,23 +96,53 @@ class Transformer(pl.LightningModule):
         )
         return mask
 
-    def __init__(self, hidden_size=256, batch_size=4, num_layers=4):
-
+    def __init__(self, hidden_size=256, num_layers=6):
+        super().__init__()
         self.hidden_size = hidden_size
-        self.batch_size = 4
         self.num_layers = num_layers
         self.encoder_layer = nn.TransformerEncoderLayer(
             d_model=self.hidden_size, nhead=8
         )
+
+        self.positional_encoding = PositionalEncoding(hidden_size=self.hidden_size)
         self.transformer_encoder = nn.TransformerEncoder(
             self.encoder_layer, num_layers=num_layers
         )
-        self.positional_encoding = PositionalEncoding(d_model=self.hidden)
+        self.linear = torch.nn.Linear(self.hidden_size, 1).double()
+        self.loss_func = torch.nn.MSELoss()
 
     def forward(self, x):
 
         # seq_length,batch_size,
         batch_size, length = x.shape
-        encoded, _ = self.encoder(x.view(length, batch_size, 1).double())
-        out = self.linear(encoded[-1, :, :].view(batch_size, self.hidden_size))
-        return out
+        positionally_encoded = self.positional_encoding(x.double())
+        encoded = self.transformer_encoder(
+            positionally_encoded.view(length, batch_size, self.hidden_size),
+            mask=self.generate_square_subsequent_mask(length),
+        )
+        out = self.linear(encoded)
+        return out.view(batch_size, length, 1).squeeze(-1)
+
+    def training_step(self, batch, batch_idx):
+
+        x = batch[:, 0:-1]
+        y = batch[:, 1:]
+        y_pred = self.forward(x)
+        loss = self.loss_func(y, y_pred)
+        self.log(
+            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x = batch[:, 0:-1]
+        y = batch[:, 1:]
+        y_pred = self.forward(x)
+        loss = self.loss_func(y, y_pred)
+        self.log(
+            "val_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
--- stonks/model_tests.py
+++ stonks/model_tests.py
@@ -4,11 +4,11 @@ import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader
 import glob
-from model import AutoregressiveLstm
+from model import AutoregressiveLstm,Transformer
 
 
 FILES = list(
-    glob.iglob("/home/cehmann/projects/stonks/data/daily/us/nyse stocks/*/*txt")
+    glob.iglob("/home/cehmann/workspaces/stonks/data/daily/us/nyse stocks/*/*txt")
 )
 
 FILES = [ f for f in FILES if f not in IGNORE_LIST]
@@ -16,7 +16,7 @@ FILES = [ f for f in FILES if f not in IGNORE_LIST]
 train_dataset = StocksDataset(files=FILES[:10])
 train_dataloader = DataLoader(train_dataset,batch_size=2,shuffle=False)
 
-model = AutoregressiveLstm().double()
+model = Transformer().double()
 for batch in train_dataloader:
 
     model.training_step(batch.double(),0)
